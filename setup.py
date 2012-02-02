83b954289849bc6ca864d73024302cfddef8ab3b
Jun Kimura <jksmphone@gmail.com>
2012-02-02 21:05:34 +0900

    Add setup script

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(name="lamia",
+      version="0.1",
+      description="Cache module for python",
+      license="MIT",
+      author="Jun Kimura",
+      author_email="jksmphone@gmail.com",
+      url="http://github.com/bluele/Lamia",
+      packages = find_packages(),
+      keywords= "python cache module",
+      zip_safe = True)
